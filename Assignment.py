cf6ea747bd840aa63b5708717b6f1d66dd066ea1
Zohra Bekzad <73633699+Zohra240@users.noreply.github.com>
2023-09-15 19:15:41 +0430

    Update Assignment.py

--- Assignment.py
+++ Assignment.py
@@ -1,4 +1,4 @@
-# Scraping Shopify Website - Using BeautifulSoup
+# Scraping jvbazar Website - Using BeautifulSoup
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
@@ -6,33 +6,37 @@ import matplotlib.pyplot as plt
 
 products = []   # List to store names of the products
 prices = []     # List to store prices of the products
-ratings = []    # List to store ratings of the products
 
-sp = requests.get("https://admin.shopify.com/store/f3d03f-2/products,b5g&marketplace=SHOPIFY")
+sp = requests.get("https://jvbazar.com/shop/")
 sp = BeautifulSoup(sp.content, 'html.parser')
 
-for each in sp.find_all('a', href=True, attrs={'class':'_1fQZEK'}):
-    name = each.find('div', attrs={'class':'_4rR01T'})
-    price = each.find('div', attrs={'class':'_30jeq3 _1_WHN1'})
-    rate = each.find('div', attrs={'class':'_3LWZlK'})
-
+for each in sp.find_all('p',  attrs={'class':'name product-title woocommerce-loop-product__title'}):
+    name = each.find('a', href=True, attrs={'class':'woocommerce-LoopProduct-link woocommerce-loop-product__link'})
+    price = each.find('span', attrs={'class':'woocommerce-Price-currencySymbol'})
+     
+ 
     if name is None:    # Caters for instances where the name does not exist
         products.append(None)
     else:
         products.append(name.text) # Get the text part
 
-    if price is None:
-        prices.append(None)
-    else:
-        prices.append(price.text)
+ 
 
-    if rate is None:    # Caters for instances where the rating does not exist - which was causing an error initially
-        ratings.append(None)
-    else:
-        ratings.append(rate.text)
+text = "nbsp$: 750 nb"
+
+# Remove unwanted characters using strip
+price = text.strip('nbsp$: nb')
+
+# Check if the stripped text is numeric
+if price.isdigit():
+    prices = price
+else:
+    prices = None
+
+print(f"The extracted price is: {prices}")
 
 # Structuring and storing data
-df = pd.DataFrame({'Product Name': products, 'Price': prices, 'Rating': ratings}) 
+df = pd.DataFrame({'Product_Name': products, 'Price': prices}) 
 print(df.to_string())
 
 # Output the DataFrame to CSV file
@@ -41,10 +45,10 @@ df.to_csv('products.csv', index = False)
 # Data Visualization
 df2 = pd.read_csv("products.csv")
 
-plt.xlabel("Rating")
+plt.xlabel("Product_Name")
 plt.ylabel("Price")
 plt.title("Rating against Price")
 
-plt.scatter(df2.Rating, df2.Price, marker="*", c = 'red', alpha = 1)    # Line graph - The labels above apply for this plot only
+plt.scatter(df2.Product_Name, df2.Price, marker="*", c = 'red', alpha = 1)    # Line graph - The labels above apply for this plot only
 # marker: format can be o or * , c: color, alpha: opacity(Range: 0-1)
 plt.show()
